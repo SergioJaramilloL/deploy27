2efa221568c95e34e95fe6fd0f422e84264d0b91
Sergio Jaramillo Luna <sergiojaramillo@mac-sergio.local>
2023-03-16 09:21:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [ posts, setPosts ] = useState([])
+  const [ loading, setLoading ] = useState(false)
+  const [ error, setError ] = useState(null)
+
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`${process.env.REACT_APP_API_URL}posts`)
+      .then(( res )  => {
+        setPosts(res.data)
+      })
+      .catch((error) => {
+        setError(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if(error !== null) {
+    return <p>Ups! Ocurrió un error. Error: {error.message}. Vuelve a intentarlo</p>
+  }
+
+  return(
+    <React.Fragment>  
+      <h3>Mira los POSTS</h3>
+      {loading ? (
+        <p>Loading...</p>
+      ) : 
+      posts.map((item) => {
+        return(
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.body}</p>
+          </div>
+        )}
+      )}
+    </React.Fragment>
+  )
 }
 
 export default App;
